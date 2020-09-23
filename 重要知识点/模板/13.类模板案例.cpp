6d93e2320fa967ee6774433f887566d5165d18bf
MattiaFang <fangwuchen1@163.com>
2020-09-23 10:45:55 +0800

    9/23

--- 重要知识点/模板/13.类模板案例.cpp
+++ 重要知识点/模板/13.类模板案例.cpp
@@ -1,6 +1,7 @@
 //2020.09.22
 //类模板案例：实现一个通用的数组类
-/*可以对内置数据类型以及自定义数据类型的数据进行存储
+/*
+可以对内置数据类型以及自定义数据类型的数据进行存储
 将数据中数据存储到堆区
 构造函数中可以传入数组的容量
 提供对应的拷贝函数以及operator=防止浅拷贝问题
@@ -12,17 +13,90 @@
 #include<string>
 #include "MyArray.hpp"
 using namespace std;
+
+void printIntArray(MyArray <int>& arr)
+{
+	for(int i=0;i<arr.getSize();i++)
+	{
+		cout<<arr[i]<<endl;
+	}
+}
 void test01()
 {
 	MyArray<int>arr1(5);
 
+	for(int i=0;i<5;i++)
+	{
+		//利用尾插法向数组插入数据
+		arr1.Push_Back(i);
+	}
+	cout<<"arr1 的打印输出为："<<endl;
+	printIntArray(arr1);
+
+	cout<<"arr1的容量为："<<arr1.getCapacity()<<endl;
+	cout<<"arr1的大小为："<<arr1.getSize()<<endl;
+
 	MyArray<int>arr2(arr1);
 
-	MyArray<int>arr3(100);
-	arr3 = arr1;
+	cout <<"arr2的打印输出为："<<endl;
+	printIntArray(arr2);
+
+	//尾删
+	arr2.Pop_Back();
+	cout<<"arr2尾删后："<<endl;
+	cout<<"arr2的容量为："<<arr2.getCapacity()<<endl;
+	cout<<"arr2的大小为："<<arr2.getSize()<<endl;
+	//MyArray<int>arr3(100);
+	//arr3 = arr1;
 }
+
+//测试 自定义数据类型
+class Person
+{
+public:
+	Person()           //VS2012 一定要把默认构造器写完整！！
+	{
+	}
+	Person(string name,int age)
+	{
+		this->m_Name = name;
+		this->m_Age  = age;
+	}
+	string m_Name;
+	int m_Age;
+};
+void printPersonArray(MyArray<Person>& arr)
+{
+	for(int i=0;i<arr.getSize();i++)
+	{
+		cout<<"姓名："<<arr[i].m_Name<<"、年龄："<<arr[i].m_Age<<endl;
+	}
+}
+void test02()
+{
+	MyArray<Person> arr(10);
+	Person p1("孙悟空",999);
+	Person p2("安琪拉",21);
+	Person p3("刘备",29);
+	Person p4("关羽",19);
+	Person p5("赵云",20);
+
+	//将数据插入到数组中
+	arr.Push_Back(p1);
+	arr.Push_Back(p2);
+	arr.Push_Back(p3);
+	arr.Push_Back(p4);
+	arr.Push_Back(p5);
+
+	//打印数组
+	printPersonArray(arr);
+	cout<<"arr的容量为："<<arr.getCapacity()<<endl;
+	cout<<"arr的大小为："<<arr.getSize()<<endl;
+}
+
 int main()
 {
+	//test01();
 	test01();
 	system("pause");
 	return 0;
--- 重要知识点/模板/MyArray.hpp
+++ 重要知识点/模板/MyArray.hpp
@@ -10,7 +10,7 @@ public:
 	//有参构造 参数 容量
 	MyArray(int capacity)
 	{
-		cout<<"MyArray的有参构造调用"<<endl;
+		//cout<<"MyArray的有参构造调用"<<endl;
 		this->m_Capacity =capacity;
 		this->m_Size = 0;
 		this->pAddress = new T[this->m_Capacity];
@@ -18,7 +18,7 @@ public:
 	//拷贝构造
 	MyArray(const MyArray& arr)
 	{
-		cout<<"MyArray的拷贝构造调用"<<endl;
+		//cout<<"MyArray的拷贝构造调用"<<endl;
 		this->m_Capacity = arr.m_Capacity;
 		this->m_Size = arr.m_Size;
 		//this->pAddress =arr.pAddress;
@@ -35,7 +35,7 @@ public:
 	//operator 防止浅拷贝问题
 	MyArray & operator= (const MyArray& arr)
 	{
-		cout<<"MyArray的operator=函数调用"<<endl;
+		//cout<<"MyArray的operator=函数调用"<<endl;
 		//先判断原来堆区是否有数据，如有先释放
 		if(this->pAddress!=NULL)
 		{
@@ -55,10 +55,51 @@ public:
 		return *this;
 	}
 
+	//尾插法
+	void Push_Back(const T & val)
+	{
+		//判断容量是否等于大小
+		if(this->m_Capacity == this->m_Size)
+		{
+			return;
+		}
+		this->pAddress[this->m_Size] = val;//在数组末尾插入数据
+		this->m_Size++;
+	}
+
+	//尾删法
+	void Pop_Back()
+	{
+		//让用户访问不到最后一个元素，即为尾删，逻辑删除
+		if(this->m_Size == 0)
+		{
+			return;
+		}
+		this->m_Size--;
+	}
+
+	//通过下标方式访问数组中的元素
+	T& operator[](int index)
+	{
+		return this->pAddress[index];
+	}
+
+	//返回数组容量
+	int getCapacity()
+	{
+		return this->m_Capacity;
+	}
+
+	//返回数组大小
+	int getSize()
+	{
+		return this->m_Size;
+	}
+
 	//析构函数
 	~MyArray()
 	{
-		cout<<"MyArray的析构函数调用"<<endl;
+		//cout<<"MyArray的析构函数调用"<<endl;
 		if(this->pAddress!=NULL)
 		{
 			delete[] this->pAddress;
