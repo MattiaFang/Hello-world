a3a000c6adb9bf8ef112f330108b8618a0ad5d48
MattiaFang <fangwuchen1@163.com>
2020-09-29 00:00:40 +0800

    9/28

---
+++ 重要知识点/STL/12.vector构造函数.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void printVector(vector<int>&v)//这里引用传递首地址，不然拷贝传值会需要占用很大内存
+{
+	for(vector<int>::iterator it =v.begin();it!=v.end();it++)
+	{
+		cout <<*it<<" ";
+	}
+	cout<<endl;
+}
+//vector 容器构造
+void test01()
+{
+	vector<int>v1; //默认构造 无参构造
+	for(int i=0;i<10;i++)
+	{
+		v1.push_back(i);
+	}
+	printVector(v1);
+
+	//通过区间的方式进行构造
+	vector<int>v2(v1.begin(),v1.end());
+	printVector(v2);
+
+	//n个elem方式构造
+	vector<int>v3(10,100);//10个100
+	printVector(v3);
+
+	//拷贝构造
+	vector<int>v4(v3);
+	printVector(v4);
+}
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
