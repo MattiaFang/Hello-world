b1e31a800521857362649eb26e09519ff7036f99
MattiaFang <fangwuchen1@163.com>
2020-10-01 23:55:43 +0800

    10/01国庆中秋快乐

---
+++ 重要知识点/STL_PLUS/01.stack容器.cpp
@@ -0,0 +1,33 @@
+//2020.10.01
+#include<iostream>
+#include<stack>
+using namespace std;
+
+//栈stack容器
+void test01()
+{
+	//特点:符合先进后出数据结构
+	stack<int>s;
+
+	//入栈
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	s.push(40);
+	cout << "栈的大小为： "<<s.size()<<endl;
+	//只要栈不为空，查看栈顶并且执行出栈操作
+	while(!s.empty())
+	{
+		//查看栈顶元素
+		cout<<"栈顶元素为："<<s.top()<<endl;
+
+		//出栈
+		s.pop();
+	}
+}
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
